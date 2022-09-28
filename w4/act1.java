7083dbd8dbbaefd808aed15a6da718b87082d0f8
neikhn <neikh.ng@gmail.com>
2022-09-28 09:06:23 +0700

    da xoa w3 w4

--- w4/act1.java
+++
@@ -1,9 +0,0 @@
-package week4; 
-public class act1 {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i ++ ) {
-            System.out.print(i);
-            System.out.print("\n");
-        }
-    }
-}
