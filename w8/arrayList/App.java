a01475d1aaf9fb1f4275017d316c1c99125c7e9b
Rek <87273844+rek3000@users.noreply.github.com>
2022-10-19 14:53:33 +0700

    Update App.java

--- w8/arrayList/App.java
+++ w8/arrayList/App.java
@@ -4,22 +4,22 @@ public class App {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the number of elements of the array: ");
 		int n = input.nextInt();
-		int[] def = new int[n];
+		int[] list = new int[n];
 
 		System.out.print("Enter the elements of the array: ");
 		for (int i = 0; i < n; i++) {
-			def[i] = input.nextInt();	
+			list[i] = input.nextInt();	
 		}
 
-		count(def);
+		count(list);
 	}
 
-	public static void count(int[] def) {	
+	public static void count(int[] list) {	
 		int[] out = new int[10];
 		System.out.println(Arrays.toString(out));
 
-		for (int i = 0; i < def.length; i++) {
-		out[def[i] % 10]++;
+		for (int i = 0; i < list.length; i++) {
+		out[list[i] % 10]++;
 		}
 
 		System.out.println(Arrays.toString(out));
