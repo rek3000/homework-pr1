ae7682442ea93686e6ecf74db1308aa3a96d7549
rek3000 <showmeeverybody@gmail.com>
2022-11-16 15:35:04 +0700

    tut11 added

--- tut10/Main.java
+++ tut10/Main.java
@@ -4,6 +4,7 @@
 import java.util.*;
 import java.io.*;
 
+@SuppressWarnings("unchecked")
 public class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
